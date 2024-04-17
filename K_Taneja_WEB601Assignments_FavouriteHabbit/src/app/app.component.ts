148df530cde3914875d496d6bf032335a380b3aa
kashishtaneja17 <Kashishtaneja17@gmail.com>
2024-04-17 05:41:13 -0400

    commit

--- K_Taneja_WEB601Assignments_FavouriteHabbit/src/app/app.component.ts
+++ K_Taneja_WEB601Assignments_FavouriteHabbit/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ContentCardComponent } from './content-card/content-card.component';
+//import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from "./content-list/content-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, ContentCardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, ContentListComponent]
 })
 export class AppComponent {
   title = 'K_Taneja_WEB601Assignments_FavouriteHabbit';
